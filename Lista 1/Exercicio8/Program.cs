5d694e642d0cec7f36c2c45c934fe69c4763d927
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-06-16 17:32:02 -0300

    Lista 1

---
+++ Lista 1/Exercicio8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius. A fórmula de conversão é: C = K − 273.15, sendo C a temperatura em Celsius e K a temperatura em Kelvin.
+
+            double temp, convertido;
+            Console.WriteLine("Qual temperatura em Kelvin você gostaria de saber em Celsius?");
+            temp = double.Parse(Console.ReadLine());
+
+            convertido = temp - 273.15;
+
+            Console.WriteLine("A temperatura de Celsius é " + convertido);
+        }
+    }
+}
