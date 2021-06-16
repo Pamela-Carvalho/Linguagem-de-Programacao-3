d98cc23f5f0e157944ec0edaf8b7525003348ee3
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-06-16 17:28:13 -0300

    Lista 1

---
+++ Lista 1/Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça uma programa que leia um número inteiro e o imprima.
+
+            int num;
+
+            Console.WriteLine("Insira um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o número " + num + ".");
+        }
+    }
+}
