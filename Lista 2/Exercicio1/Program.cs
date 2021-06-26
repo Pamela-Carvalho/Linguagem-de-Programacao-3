fc4316a651dff529f4ab379f21e2725151cb629e
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-06-26 12:57:15 -0300

    Lista 2

---
+++ Lista 2/Exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Insira um número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira outro número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine(num1 + " é maior do que " + num2);
+            else
+                Console.WriteLine(num2 + " é maior do que " + num1);
+        }
+    }
+}
---
+++ Lista 2/Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Insira um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("O quadrado de " + num + " é " + Math.Pow(num, 2));
+                Console.WriteLine("A raiz quadrada de " + num + " é " + Math.Sqrt(num));
+            }
+        }
+    }
+}
