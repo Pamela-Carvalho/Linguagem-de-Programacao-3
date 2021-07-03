cbe3d4959d0064c5d9cc7430fdf05da2e09fb4b6
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-03 20:59:10 -0300

    Lista 3

---
+++ Lista 3/Exercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Insira um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nOs " + num + " primeiros ímpares são: ");
+
+            int cont = 0;
+
+            do {
+                cont++;
+
+                if ((cont % 2) == 1)
+                {
+                    Console.WriteLine(cont);
+                    num--;
+                }
+            }
+            while (num > 0);
+        }
+    }
+}
---
+++ Lista 3/Exercicio7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Insira um número inteiro positivo par: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nOs números pares de 0 até " + num + " são: ");
+
+            int cont = 0;
+
+            do
+            {
+                cont++;
+
+                if ((cont % 2) == 0)
+                    Console.WriteLine(cont);
+            }
+            while (cont < num);
+        }
+    }
+}
