c77315e1832931e6e0eaa20313fb2ce09bbdb72d
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-03 18:49:09 -0300

    Lista 2

---
+++ Lista 2/Exercicio11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float trabalho, avaliacao, exame, media;
+
+            Console.WriteLine("Insira as respestivas notas com um valor de 0 a 10.");
+            Console.WriteLine("Trabalho de laboratório: ");
+            trabalho = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Avaliação semestral: ");
+            avaliacao = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Exame final: ");
+            exame = float.Parse(Console.ReadLine());
+
+            media = ((trabalho * 2) + (avaliacao * 3) + (exame * 5)) / 10;
+
+            Console.WriteLine("Nota final: " + media);
+
+            if ((media >= 5) && (media <= 10))
+                Console.WriteLine("O aluno foi aprovado.");
+            else if ((media >= 3) && (media <= 4.9))
+                Console.WriteLine("O aluno está de recuperação.");
+            else if ((media >= 0) && (media <= 2.9))
+                Console.WriteLine("O aluno foi reprovado.");
+        }
+    }
+}
---
+++ Lista 2/Exercicio13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Calcule a área de um trapézio-------");
+            float bmenor, bmaior, altura, area;
+
+            Console.WriteLine("Base menor: ");
+            bmenor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Base maior: ");
+            bmaior = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            area = ((bmenor + bmaior) * altura) / 2;
+
+            Console.WriteLine("A área é " + area);
+        }
+    }
+}
