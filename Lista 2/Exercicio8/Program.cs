651f34cf836fa07c1eec0f7892ed3d0e26baeaa2
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-03 16:39:04 -0300

    Lista 2

---
+++ Lista 2/Exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            string sexo;
+
+            Console.WriteLine("-----------Calcule seu peso ideal-----------");
+            Console.WriteLine("Altura (em metros): ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sexo (digite 'feminino' ou 'masculino'): ");
+            sexo = Console.ReadLine();
+
+            if (sexo.ToLower() == "masculino")
+                Console.WriteLine("O peso ideal é " + ((72.7 * altura) - 58.0));
+            else if (sexo.ToLower() == "feminino")
+                Console.WriteLine("O peso ideal é " + ((62.1 * altura) - 44.7));
+            else
+                Console.WriteLine("O dado informado é inválido");
+        }
+    }
+}
---
+++ Lista 2/Exercicio9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor;
+            int soma = 0;
+
+            Console.WriteLine("Insira um número inteiro maior que zero: ");
+            valor = Console.ReadLine();
+
+            if (int.Parse(valor) > 0) {
+                foreach (char x in valor)
+                {
+                    soma += (int)char.GetNumericValue(x);
+                }
+
+                Console.Write("A soma dos algarismos é: " + soma);
+            }
+            else
+                Console.WriteLine("Número inválido");
+        }
+    }
+}
