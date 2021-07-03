b43c5f6dad6ca2cf9b9179151ebcd84cea271427
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-03 16:37:36 -0300

    Lista 2

---
+++ Lista 2/Exercicio6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1, nota2;
+
+            Console.WriteLine("Insira a primeira nota: ");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a segunda nota: ");
+            nota2 = float.Parse(Console.ReadLine());
+
+            if((nota1 >= 0.0) && (nota1 <= 10.0)) {
+                if ((nota2 >= 0.0) && (nota2 <= 10.0))
+                {
+                    Console.WriteLine("A média do(a) aluno(a) é " + (nota1 + nota2) / 2);
+                }
+                else
+                    Console.WriteLine("A segunda nota é inválida.");
+            }
+            else
+                Console.WriteLine("A primeira nota é inválida.");
+        }
+    }
+}
---
+++ Lista 2/Exercicio7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, emprestimo;
+
+            Console.WriteLine("Salário: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor da prestação do empréstimo: ");
+            emprestimo = double.Parse(Console.ReadLine());
+
+            if (emprestimo > ((20.0 / 100.0) * salario))
+                Console.WriteLine("Empréstimo não concedido");
+            else
+                Console.WriteLine("Empréstimo concedido");
+        }
+    }
+}
