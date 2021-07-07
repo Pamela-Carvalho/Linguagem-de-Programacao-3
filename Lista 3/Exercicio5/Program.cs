f86ec7ce2841b11a014a113f613b7331ade19679
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-07 19:39:57 -0300

    Lista 3

---
+++ Lista 3/Exercicio5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool aux = false;
+
+            Console.WriteLine("Insira um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nO primeiro múltiplo de 11, 13 ou 17 é ");
+            for(num = num + 1; aux == false; num++) { 
+                if(num % 11 == 0) {
+                    Console.Write(num + ", um múltiplo de 11");
+                    aux = true;
+                }
+                else if (num % 13 == 0)
+                {
+                    Console.Write(num + ", um múltiplo de 13");
+                    aux = true;
+                }
+                else if (num % 17 == 0)
+                {
+                    Console.Write(num + ", um múltiplo de 17");
+                    aux = true;
+                }
+            }
+
+            //int num;
+            //int mult = 0;
+
+            //Console.WriteLine(" Entre com um número inteiro: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //for(int  i = num + 1 ; mult == 0; i++ )
+            //{
+                //mult = (i % 11 == 0) || (i % 13 == 0) || (i % 17 == 0) ? i : 0;
+            //}
+            //Console.WriteLine(" O primeiro número principal múltiplo de 11, 13 ou 17 é: " + mult);
+        }
+    }
+}
---
+++ Lista 3/Exercicio6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux, menor, maior;
+
+            Console.WriteLine("Para encerrar forneça um valor negativo.");
+            Console.WriteLine("Insira um número: ");
+            aux = int.Parse(Console.ReadLine());
+            menor = aux;
+            maior = aux;
+
+            if(aux >= 0) {
+                do
+                {
+                    Console.WriteLine("Insira um número: ");
+                    aux = int.Parse(Console.ReadLine());
+
+                    if(aux >= 0) {
+                        if (aux < menor)
+                            menor = aux;
+
+                        if (aux > maior)
+                            maior = aux;
+                    }
+
+                }
+                while (aux >= 0);
+            }
+
+            Console.WriteLine("Menor número: " + menor);
+            Console.WriteLine("Maior número: " + maior);
+        }
+    }
+}
