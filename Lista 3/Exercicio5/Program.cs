fae4f0bef4268fd6bfdff2f5e477955c18a725b4
Pâmela Carvalho <71795901+Pamela-Carvalho@users.noreply.github.com>
2021-07-07 19:40:52 -0300

    Lista 3

--- Lista 3/Exercicio5/Program.cs
+++ Lista 3/Exercicio5/Program.cs
@@ -29,18 +29,6 @@ namespace Exercicio5
                     aux = true;
                 }
             }
-
-            //int num;
-            //int mult = 0;
-
-            //Console.WriteLine(" Entre com um número inteiro: ");
-            //num = int.Parse(Console.ReadLine());
-
-            //for(int  i = num + 1 ; mult == 0; i++ )
-            //{
-                //mult = (i % 11 == 0) || (i % 13 == 0) || (i % 17 == 0) ? i : 0;
-            //}
-            //Console.WriteLine(" O primeiro número principal múltiplo de 11, 13 ou 17 é: " + mult);
         }
     }
 }
